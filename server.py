ad7d39e3497fc79badc9d23fd39a6472c8558f65
Jewel <Juliana.Furgala@tufts.edu>
2018-11-28 21:57:38 -0500

    Added game-wide client broadcast specifically for game board after processing incoming data

--- server.py
+++ server.py
@@ -13,47 +13,51 @@ class Server(object):
         self.number = 0
         self.lock = threading.Lock()
         self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # TODO: make a dictionary of connections, one sublist for each game
+        self.conns = []
 
     def handle_client(self, addr, conn, data):
         # send character name followed by the map
         conn.send("You're playing as " + data[0] + ".")
         conn.send(data[1])
         while 1:
-            data = conn.recv(20)
-            if not data:
+            inData = conn.recv(20)
+            if not inData:
                 break
-            print "received data:", data
-            conn.send(data)
+            print "received data:", inData
+            # TODO: act based on message format; call appropriate function
+            # conn.send("Success") # send success or failure message
+            for connection in self.conns:
+                connection.send(data[1])  # broadcast the updated board to all clients
         conn.close()
 
     def run(self):
         addrToGame = {}
-        conns = []
         addrList = []
 
         while 1:
             while self.number < 3:
                 incomingConn, incomingAddr = self.s.accept()
                 if incomingAddr not in addrToGame:
-                    conns.append(incomingConn)
+                    self.conns.append(incomingConn)
                     addrList.append(incomingAddr)
                     addrToGame[incomingAddr] = 1
                     self.number += 1
                     print 'Connection address:', incomingAddr
-            for conn in conns:
+            for conn in self.conns:
                 conn.send('Game can start now!')
             break
 
-        self.initiateGame(conns, addrList)
+        self.initiateGame(addrList)
 
-    def initiateGame(self, conns, addrList):
+    def initiateGame(self, addrList):
         threads = []
         game = Game(addrList)
 
-        for i in range(len(conns)):
+        for i in range(len(self.conns)):
             threads.append(threading.Thread(target=self.handle_client,
                                             args=(addrList[i],
-                                                  conns[i],
+                                                  self.conns[i],
                                                   [game.getChar(addrList[i]), game.map])))
             threads[-1].start()
 
