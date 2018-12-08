4ee0be807e46f4f36475ce7716f08fd9866ce195
caophonghoang <caophonghoang@yahoo.com>
2018-12-07 21:59:58 -0500

    Add files via upload

--- game_data.py
+++ game_data.py
@@ -113,3 +113,35 @@ MAP =  "+=======================================================================
        "|                   ---------                   ---------               |\n" \
        "|                   | c |   |                   |   |   |               |\n" \
        "+=======================================================================+"
+
+location = {(x,y) for x in range(18) for y in range(18)}
+location.difference_update({(x,y) for x in range(3) for y in range(5)})
+location.difference_update({(0,y) for y in range(7,11,1)})
+location.difference_update({(x,y) for y in range(6,12,1) for x in range(1,4,1)})
+location.difference_update({(x,y) for x in range(3) for y in range(14,18,1)})
+location.difference_update({(3,y) for y in range(15,18,1)})
+
+location.difference_update({(x,y) for x in range(6,11,1) for y in range(5)})
+location.difference_update({(x,y) for x in range(6,11,1) for y in range(7,12,1)})
+location.difference_update({(x,y) for x in range(5,8,1) for y in range(14,18,1)})
+location.difference_update({(x,y) for x in range(10,13,1) for y in range(14,18,1)})
+location.difference_update({(x,y) for x in range(14,18,1) for y in range(5)})
+location.difference_update({(x,y) for x in range(13,18,1) for y in range(7,12,1)})
+location.difference_update({(x,y) for x in range(15,18,1) for y in range(14,18,1)})
+
+location.add((2,3))
+location.add((2,6))
+location.add((2,11))
+location.add((3,7))
+location.add((3,10))
+location.add((3,15))
+location.add((5,14))
+location.add((7,4))
+location.add((7,16))
+location.add((10,3))
+location.add((10,15))
+location.add((11,14))
+location.add((13,9))
+location.add((14,4))
+location.add((14,11))
+location.add((15,14)) 
